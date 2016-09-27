b7ad70e33f111d4135cf55f6cde123f501bcbe5f
Tyler Smith <tyler.smith6891@gmail.com>
2016-09-26 17:07:35 -0700

    tabs working and added some color

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -3,14 +3,13 @@
 var projectView = {};
 
 projectView.handleMainNav = function() {
-  $('.main-nav .tab:last').on('click', function() {
-    console.log('click');
-    $('#projects').fadeOut();
-    $('#about').fadeIn();
+  $('nav').on('click', 'ul li:lt(2)', function() {
+    $('#projects').hide();
+    $('#about').hide();
+    var $text = $(this).text().toLowerCase();
+    console.log($text);
+    $('#' + $text).show();
   });
-  $('.main-nav .tab:first').on('click', function() {
-    $('#about').fadeOut();
-    $('#projects').fadeIn();
-  });
-  $('.main-nav .tab:first').click();
+  $('nav .tab:first').click(); // not working yet
 };
+projectView.handleMainNav();
