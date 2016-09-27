f7c76f2261d372b7bbec2f64250a6f29199c7efc
Tyler Smith <tyler.smith6891@gmail.com>
2016-09-27 16:23:55 -0700

    moved projects to json file and started refactoring code for json/ajax

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// empty to store created project objects
-var projects = [];
-
 // constructor function to create projects
 function Project(opts) {
   this.title = opts.title;
@@ -12,28 +9,30 @@ function Project(opts) {
   this.description = opts.description;
 }
 
+Projects.all = [];
+
 Project.prototype.toHtml = function() {
 
   var source = $('#projects-template').html();
   var template = Handlebars.compile(source);
 
-  // var $newProject = $('#projects .template').clone();
-  //
-  // $newProject.find('.author').html(this.author);
-  // $newProject.find('h1').html(this.title);
-  // $newProject.find('.project_body').html(this.description);
-  // $newProject.find('time').html(this.published);
-  //
-  //
-  // $newProject.removeClass('template');
-  //
-  // return $newProject;
   this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.published ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  var html = template(this);
-  return html;
+  var html = template(this); // These lines can
+  return html;               //can be consolidated
 };
+
+Project.loadAll = function(passedData) {
+  forEach(function(ele) {
+    Project.all.push(new Project(ele));
+  });
+};
+
+Project.fetchAll = function() {
+  if(localStorage.)
+};
+
 projectData.sort(function(curElem, nextElem) {
   return (new Date(nextElem.published)) - (new Date(curElem.published));
 });
