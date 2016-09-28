a045f417b35435cd79ecdbe323f80a4319ecd0fa
Tyler Smith <tyler.smith6891@gmail.com>
2016-09-28 16:26:51 -0700

    refactored for functional programming

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,70 +1,99 @@
 'use strict';
 
-// constructor function to create projects
-function Project(opts) {
-  this.title = opts.title;
-  this.author = opts.author;
-  this.repoUrl = opts.repoUrl;
-  this.published = opts.published;
-  this.description = opts.description;
-}
+(function(module) {
 
-Project.allProjects = [];
-
-Project.prototype.toHtml = function() {
-
-  var source = $('#projects-template').html();
-  var template = Handlebars.compile(source);
-
-  this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
-  this.publishStatus = this.published ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-
-  var html = template(this); // These lines can
-  return html;               //can be consolidated
-};
-
-Project.loadAll = function(passedData) {
-  Project.allProjects = passedData.map(function(ele) {
-    return new Project(ele);
-  });
-};
-
-// continue refactoring here
-Project.fetchAll = function(nextFunction) {
-  if(localStorage.portfolioProjects) {
-    var storedData = JSON.parse(localStorage.getItem('portfolioProjects'));
-    Project.loadAll(storedData);
-    console.log('Stored data loaded', storedData);
-  } else {
-    $.ajax('/data/portfolioProjects.json', {
-      method: 'GET',
-      success: successHandler,
-      error: errorHandler
-    });
+  // constructor function to create projects
+  function Project(opts) {
+    this.title = opts.title;
+    this.author = opts.author;
+    this.repoUrl = opts.repoUrl;
+    this.published = opts.published;
+    this.description = opts.description;
   }
-};
 
-function successHandler(data) {
-  Project.loadAll(data);
-  localStorage.setItem('portfolioProjects', JSON.stringify(data));
-  console.log('Data retrieved:', data);
-  projectView.renderIndexPage();
-}
+  Project.allProjects = [];
 
-function errorHandler(error) {
-  console.log('ERROR!', error);
-}
+  Project.prototype.toHtml = function() {
 
-Project.fetchAll();
+    var source = $('#projects-template').html();
+    var template = Handlebars.compile(source);
 
-// projectData.sort(function(curElem, nextElem) {
-//   return (new Date(nextElem.published)) - (new Date(curElem.published));
-// });
-//
-// projectData.forEach(function(ele) {
-//   projects.push(new Project(ele));
-// });
-//
-// projects.forEach(function(a) {
-//   $('#projects').append(a.toHtml());
-// });
+    this.daysAgo = parseInt((new Date() - new Date(this.published)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.published ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+
+    var html = template(this); // These lines can
+    return html;               //can be consolidated
+  };
+
+  Project.loadAll = function(passedData) {
+    Project.allProjects = passedData.map(function(ele) {
+      return new Project(ele);
+    });
+  };
+
+  Project.fetchAll = function(nextFunction) {
+    if (localStorage.portfolioProjects) {
+      $.ajax({
+        type: 'HEAD',
+        url: '/data/portfolioProjects.json',
+        success: function(data, message, xhr) {
+          var eTag = xhr.getResponseHeader('eTag');
+          if(!localStorage.eTag || eTag !== localStorage.eTag) {
+            localStorage.eTag = eTag;
+            Project.getAll(nextFunction);
+          } else {
+            Project.loadAll(JSON.parse(localStorage.portfolioProjects));
+            nextFunction();
+          }
+        }
+      });
+    } else {
+      Project.getAll(nextFunction);
+    }
+    // if(localStorage.portfolioProjects) {
+    //   var storedData = JSON.parse(localStorage.getItem('portfolioProjects'));
+    //   Project.loadAll(storedData);
+    //   console.log('Stored data loaded', storedData);
+    // } else {
+    //   $.ajax('/data/portfolioProjects.json', {
+    //     method: 'GET',
+    //     success: successHandler,
+    //     error: errorHandler
+    //   });
+    // }
+  };
+
+  Project.getAll = function(nextFunction) {
+    $.getJSON('/data/portfolioProjects.json', function(responseData) {
+      Project.loadAll(responseData);
+      localStorage.portfolioProjects = JSON.stringify(responseData);
+      nextFunction();
+    });
+  };
+
+  // function successHandler(data) {
+  //   Project.loadAll(data);
+  //   localStorage.setItem('portfolioProjects', JSON.stringify(data));
+  //   console.log('Data retrieved:', data);
+  //   projectView.renderIndexPage();
+  // }
+  //
+  // function errorHandler(error) {
+  //   console.log('ERROR!', error);
+  // }
+
+  // Project.fetchAll();
+
+  // projectData.sort(function(curElem, nextElem) {
+  //   return (new Date(nextElem.published)) - (new Date(curElem.published));
+  // });
+  //
+  // projectData.forEach(function(ele) {
+  //   projects.push(new Project(ele));
+  // });
+  //
+  // projects.forEach(function(a) {
+  //   $('#projects').append(a.toHtml());
+  // });
+  module.Project = Project;
+})(window);
